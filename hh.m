a0fe2b18edd85101ab2b81f87a0227f2aca723cc
xiaohuahao <1169756883@qq.com>
2015-07-15 11:15:02 +0800

    dd
    dd

---
+++ hh.m
@@ -0,0 +1,9 @@
+// hh
+// jj
+//
+// IDECodeSnippetCompletionScopes: [CodeBlock]
+// IDECodeSnippetIdentifier: F8A52BE6-860D-4285-95E9-09A401A0541F
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 0
+self.view.backgroundColor = [UIColor blackColor];
