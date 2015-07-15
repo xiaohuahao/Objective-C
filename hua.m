e7a8f5ff879b104c1d21ae5316ef35ff7b541c88
xiaohuahao <1169756883@qq.com>
2015-07-15 11:04:04 +0800

    hao
    duoyige

---
+++ hua.m
@@ -0,0 +1,9 @@
+// hua
+// hao
+//
+// IDECodeSnippetCompletionScopes: [CodeBlock]
+// IDECodeSnippetIdentifier: DF32F88E-7ACA-49F6-80DF-11F9FEC4A5AF
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 0
+NSLog(@"scale = %f",scale);
