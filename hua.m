ec609d48922840b3eb749264d568a865eb401902
xiaohuahao <1169756883@qq.com>
2015-07-15 11:09:56 +0800

    buy
    xz

--- hua.m
+++
@@ -1,9 +0,0 @@
-// hua
-// hao
-//
-// IDECodeSnippetCompletionScopes: [CodeBlock]
-// IDECodeSnippetIdentifier: DF32F88E-7ACA-49F6-80DF-11F9FEC4A5AF
-// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
-// IDECodeSnippetUserSnippet: 1
-// IDECodeSnippetVersion: 0
-NSLog(@"scale = %f",scale);
